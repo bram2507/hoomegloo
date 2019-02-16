4292558412f2f36c8f49bd7ab609733df12f34fc
bram2507 <abrahamalvarezgomez2507@gmail.com>
2019-02-16 04:15:43 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,41 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform,  ModalController, NavParams } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
+
+
+import { SplashPage } from '../pages/splash/splash';
+
+import { HomePage } from '../pages/home/home';
+
+import { timer } from 'rxjs/observable/timer';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage:any = SplashPage; // -- Change to SplashPage to see splash screen first -- //
+  
+  showSplash = true; // <-- show animation
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, modalCtrl: ModalController) {
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();
+      //splashScreen.hide();  //-- Uncomment to hide the splash screen: intial config--//
+      
+      let splash = modalCtrl.create(SplashPage); //-- Uncomment to show the splash screen --//
+      splash.present();                          //-- Uncomment to show the splash screen --// 
+       
+       timer(6000).subscribe(() => this.rootPage = HomePage) // <-- hide animation after 3s
+      
     });
   }
+  
+  
+  
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ViewController, Img, TapClick } from 'ionic-angular';
+import { getDOM } from '@angular/platform-browser/src/dom/dom_adapter';
+import { ClassMethod } from '@angular/compiler/src/output/output_ast';
+import { elementStart } from '@angular/core/src/render3/instructions';
+import { style } from '@angular/core/src/animation/dsl';
+import { StatusBar } from '@ionic-native/status-bar';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +12,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  passwordType: string = 'password';
+  passwordIcon: any;
+  flag: boolean = true;
+  mflag: boolean = true;
 
-  }
+  constructor(public navCtrl: NavController, public viewCtrl: ViewController) { }
 
+   // -- Show and hide the password when eye icon is clicked -- //
+   tooglePassword(eye_closed:any){ 
+      let img: HTMLImageElement;
+      img= eye_closed;
+      img.src="../assets/imgs/ionicons-svg-md-eye-off.svg";
+
+      this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+      
+      if (this.flag == true){
+        img.src="../assets/imgs/ionicons-svg-md-eye.svg"; this.flag = false; 
+      } else {
+        img.src="../assets/imgs/ionicons-svg-md-eye-off.svg"; this.flag = true;
+      }
+   }
+
+   // -- Slide left and right hamburger Menu  hamburger icon is clicked-- //
+   hamburgerMenu(menu:any){
+
+
+      
+      if (this.mflag == true){
+        document.getElementById("nav").style.transform="translateX(0%)";
+        this.mflag = false; 
+        document.getElementById("spn").style.boxShadow="none";
+        
+        
+      } else {
+        document.getElementById("nav").style.transform="translateX(-120%)";
+        this.mflag = true;
+        
+        document.getElementById("spn").style.boxShadow="0.08em 0.08em #999";
+      }
+   }
+
+ 
 }
